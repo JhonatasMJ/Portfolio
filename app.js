ffc9a6322a338ad7b1e3e5e4c94308510216d941
JhonatasMJ <jhonatasjhmj@gmail.com>
2023-08-02 11:27:19 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => (elemento.innerHTML += letra), 75 * i);
+  });
+}
+
+const titulo = document.querySelector(".titulo");
+typeWriter(titulo);
+
+/* Menu Mobile */
+
+const btnMobile = document.getElementById("hamburguer");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
